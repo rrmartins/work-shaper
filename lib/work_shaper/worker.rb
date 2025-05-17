e0717f3638420f54dbe2a276d59d86293ac5127e
Rodrigo Martins <rrmartinsjg@gmail.com>
2025-05-17 02:25:51 +0100

    Add LoggerFactory inclusion to Manager and Worker classes

--- lib/work_shaper/worker.rb
+++ lib/work_shaper/worker.rb
@@ -1,6 +1,7 @@
 module WorkShaper
   # The worker that runs the stuff
   class Worker
+    include LoggerFactory
     # rubocop:disable Metrics/ParameterLists
     # rubocop:disable Layout/LineLength
     # @param work [Lambda] Lambda that we will #call(message) to execute work.
